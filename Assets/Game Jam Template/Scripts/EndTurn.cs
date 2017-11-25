2d69d3326da96c8362dbaf61699d71c1f81b3f64
Roberto <roberto.sanchez@openmailbox.org>
2017-11-25 20:01:50 +0100

    Implementado cambio de turno

---
+++ Assets/Game Jam Template/Scripts/EndTurn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTurn : MonoBehaviour {
+
+	public void TaskOnClick() {
+			main_behavior.index_player++;
+			main_behavior.index_player%=main_behavior.jugadores.Count;
+			Debug.Log("Jugador " + main_behavior.index_player);
+	}
+}
