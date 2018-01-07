41c1a8967a22cbc13c4c1fbfe33bd37360e57705
Roberto Sánchez <33062572+rsf92@users.noreply.github.com>
2018-01-07 22:00:20 +0100

    Parece que funciona pero va extra lento...

--- Assets/Game Jam Template/Scripts/Tile.cs
+++ Assets/Game Jam Template/Scripts/Tile.cs
@@ -14,6 +14,7 @@ public class Tile : MonoBehaviour
 	private bool panelStart;
 	private bool ret;
 	private static int valueDropdown ;
+	private static bool performing = false;
 	// Use this for initialization
 	IEnumerator Start ()
 	{
@@ -114,207 +115,210 @@ public class Tile : MonoBehaviour
 
 	IEnumerator OnMouseUp ()
 	{
-		
-		Army temporal;
-
-		if (origen != null && (me.isAdyacent(origen.me) != true && this != origen)) {
-			Debug.Log ("Sólo te puedes mover a casillas adyacentes!");
-		}
-		else if (((string)main_behavior.jugadores [main_behavior.index_player]).Contains (me.getOwner ()) == true && Tile.origen == null) {
-			if(main_behavior.reparte == true){
+		if (Tile.performing == false) {
+			Army temporal;
+			Tile.performing = true;
+			if (origen != null && (me.isAdyacent (origen.me) != true && this != origen)) {
+				Debug.Log ("Sólo te puedes mover a casillas adyacentes!");
+			} else if (((string)main_behavior.jugadores [main_behavior.index_player]).Contains (me.getOwner ()) == true && Tile.origen == null) {
+				if (main_behavior.reparte == true) {
 				
-				List<string> m_DropOptions = new List<string> ();
-				for(int i = 0; i<=main_behavior.units_hold; i++){
-					m_DropOptions.Add(i.ToString());				
-				}
-
-				GameObject panelControl =GameObject.Find ("PanelController");
-				panel = panelControl.GetComponent<PanelSoldado>();
-				panel.DoVisible();
-		
-				valueDropdown = 0;
-
-				Dropdown drpSoldados = GameObject.Find ("drpSoldados").GetComponent<Dropdown>();
-				//Clear the old options of the Dropdown menu
-				drpSoldados.ClearOptions();
-				//Add the options created in the List above
-				drpSoldados.AddOptions(m_DropOptions);
-				drpSoldados.value = 0;
-
-			}else{
-				if (me.getUnits () == 0) {
-					Debug.Log ("No se puede elegir como origen una casilla vacía!");
-				} else {
-					Debug.Log ("Elegida la casilla");
-					origen = this;
-				}
-			}
-			/*User selects this tile*/
-		} else if (((string)main_behavior.jugadores [main_behavior.index_player]).Contains (me.getOwner ()) == true && Tile.origen != null) {
-			if ((origen.me != me)) {
-				
-				Debug.Log ("Origen Iniciales" + origen.me.getUnits ());
-				Debug.Log ("Destino Iniciales" + me.getUnits ());
-				print("adyacente");
-
-				List<string> m_DropOptions = new List<string> ();
-				for(int i = 0; i<=origen.me.getUnits (); i++){
-					m_DropOptions.Add(i.ToString());				
-				}				
-
-				GameObject panelControl =GameObject.Find ("PanelControllerB");
-				panel = panelControl.GetComponent<PanelSoldado>();
-				panel.DoVisible();
-		
-				valueDropdown = 0;
-
-				Dropdown drpSoldados = GameObject.Find ("drpSoldadosB").GetComponent<Dropdown>();
-				//Clear the old options of the Dropdown menu
-				drpSoldados.ClearOptions();
-				//Add the options created in the List above
-				drpSoldados.AddOptions(m_DropOptions);
-				drpSoldados.value = 0;
-
-				yield return new WaitForSeconds (10.2f);
-
-				if(valueDropdown == 0){
-					yield return new WaitForSeconds (5.3f);
-				
-					bool stop = true;
-					while(stop){
-						if(valueDropdown > 0)
-							stop = false;
-						else
-							yield return new WaitForSeconds (5.2f);
-					}
-				}
-				print ("value drop "+valueDropdown);
-
-				ret = origen.me.put_on_hold (valueDropdown);
-				if (ret == true) {
-					origen.me.move_Units (me);
-					Debug.Log ("Origen Finales" + origen.me.getUnits ());
-					Debug.Log ("Destino Finales" + me.getUnits ());
-
-					origen.paintUnits ();
-
-					temporal = movable ();
-					Vector3 dir = getDirection (origen.me);
-					for (int i = 0; i < 20; i++) {
-						temporal.move (dir);
-						yield return new WaitForSeconds (0.05f);
+					List<string> m_DropOptions = new List<string> ();
+					for (int i = 0; i <= main_behavior.units_hold; i++) {
+						m_DropOptions.Add (i.ToString ());				
 					}
 
-					temporal.deinstantiate ();
-					paintUnits ();
-				}	
+					GameObject panelControl = GameObject.Find ("PanelController");
+					panel = panelControl.GetComponent<PanelSoldado> ();
+					panel.DoVisible ();
+		
+					valueDropdown = 0;
 
-			}
-			Tile.reset_origen ();
-			Debug.Log ("Deseleccionada la casilla");
-			/*User deselects this tile or moves to another tile*/
-
-		} else if (((string)main_behavior.jugadores [main_behavior.index_player]).Contains (me.getOwner ()) != true && Tile.origen != null) {
-			/*Attack!*/
-			int unidades = origen.me.getUnits ();
-			int numero_de_dados = 0;
-			if (unidades > 3) 
-				unidades = 3;
-
-			temporal = movable (unidades);
-
-
-
-			origen.me.put_on_hold (unidades);
-			origen.paintUnits ();
-			Vector3 dir = getDirection (origen.me);
-
-			for (int i = 0; i < 10; i++) {
-				temporal.move (dir);
-				yield return new WaitForSeconds (0.1f);
-			}
-			unidades = me.getUnits ();
-			do {
-				numero_de_dados = temporal.getUnits ();
-
-				if (unidades == 0) {
-					me.conquer (origen.me.getOwner (), me.getUnits ());
+					Dropdown drpSoldados = GameObject.Find ("drpSoldados").GetComponent<Dropdown> ();
+					//Clear the old options of the Dropdown menu
+					drpSoldados.ClearOptions ();
+					//Add the options created in the List above
+					drpSoldados.AddOptions (m_DropOptions);
+					drpSoldados.value = 0;
 
 				} else {
-
-					if (unidades > 2)
-						unidades = 2;
-
-					numero_de_dados += unidades;
-
-					DiceSwipeControl.set_num_dices (numero_de_dados);
-					GameObject diceControl = GameObject.Find ("SwipeController");
-					diceControl.GetComponent<DiceSwipeControl> ().manualStart ();
-
-					//Recovering dices result
-					yield return new WaitForSeconds (10.0f);
-					bool stop = true;	
-
-					List<int> resultados = DiceSwipeControl.results;
-					if (resultados == null || resultados.Count != numero_de_dados) {
-
-						while (stop) {
-
-							yield return new WaitForSeconds (0.2f);
-							resultados = DiceSwipeControl.results;
-							if (resultados != null && resultados.Count == numero_de_dados) {
-								stop = false;
-								foreach (int resu in resultados) {
-									Debug.Log ("resu " + resu);			
-								}
-							}
-						}
-						print ("resultado if tile " + resultados.Count);
+					if (me.getUnits () == 0) {
+						Debug.Log ("No se puede elegir como origen una casilla vacía!");
 					} else {
-						print ("resultado tile " + resultados.Count + " " + numero_de_dados);
+						Debug.Log ("Elegida la casilla");
+						origen = this;
+					}
+				}
+				/*User selects this tile*/
+			} else if (((string)main_behavior.jugadores [main_behavior.index_player]).Contains (me.getOwner ()) == true && Tile.origen != null) {
+				if ((origen.me != me)) {
+				
+					Debug.Log ("Origen Iniciales" + origen.me.getUnits ());
+					Debug.Log ("Destino Iniciales" + me.getUnits ());
+					print ("adyacente");
 
-						int breakpoint = resultados.Count / 2;
-						int[] attacker = new int[breakpoint];
-						int[] defender = new int[breakpoint];
-						bool def=true;
+					List<string> m_DropOptions = new List<string> ();
+					for (int i = 0; i <= origen.me.getUnits (); i++) {
+						m_DropOptions.Add (i.ToString ());				
+					}				
 
-						for (int i = 0; i < breakpoint; i++) {
+					GameObject panelControl = GameObject.Find ("PanelControllerB");
+					panel = panelControl.GetComponent<PanelSoldado> ();
+					panel.DoVisible ();
+		
+					valueDropdown = 0;
 
-							for (int j = breakpoint; j < breakpoint*2; j++) {
-								if(resultados[i] >= resultados[j]){
-									def=false;
-									break;
-								}
-							}
-							if(def == false){
-								me.kill_unit();
-								unidades--;
-							}else{
-								temporal.kill_unit();
-							}
+					Dropdown drpSoldados = GameObject.Find ("drpSoldadosB").GetComponent<Dropdown> ();
+					//Clear the old options of the Dropdown menu
+					drpSoldados.ClearOptions ();
+					//Add the options created in the List above
+					drpSoldados.AddOptions (m_DropOptions);
+					drpSoldados.value = 0;
 
-							def = true;
+					yield return new WaitForSeconds (10.2f);
+
+					if (valueDropdown == 0) {
+						yield return new WaitForSeconds (5.3f);
+				
+						bool stop = true;
+						while (stop) {
+							if (valueDropdown > 0)
+								stop = false;
+							else
+								yield return new WaitForSeconds (5.2f);
 						}
 					}
+					print ("value drop " + valueDropdown);
+
+					ret = origen.me.put_on_hold (valueDropdown);
+					if (ret == true) {
+						origen.me.move_Units (me);
+						Debug.Log ("Origen Finales" + origen.me.getUnits ());
+						Debug.Log ("Destino Finales" + me.getUnits ());
+
+						origen.paintUnits ();
+
+						temporal = movable ();
+						Vector3 dir = getDirection (origen.me);
+						for (int i = 0; i < 20; i++) {
+							temporal.move (dir);
+							yield return new WaitForSeconds (0.05f);
+						}
+
+						temporal.deinstantiate ();
+						paintUnits ();
+					}	
 
 				}
-			} while(temporal.getUnits () != 0 && me.getUnits () != 0);
+				Tile.reset_origen ();
+				Debug.Log ("Deseleccionada la casilla");
+				/*User deselects this tile or moves to another tile*/
 
-			if (temporal.getUnits () == 0) {
-				Debug.Log ("Pierdes");
-			} else {
-				Debug.Log ("Ganas");
-				me.conquer (origen.me.getOwner(),temporal.getUnits());
+			} else if (((string)main_behavior.jugadores [main_behavior.index_player]).Contains (me.getOwner ()) != true && Tile.origen != null) {
+				/*Attack!*/
+				int unidades = origen.me.getUnits ();
+				int numero_de_dados = 0;
+				if (unidades > 3)
+					unidades = 3;
+
+				temporal = movable (unidades);
+
+
+
+				origen.me.put_on_hold (unidades);
+				origen.paintUnits ();
+				Vector3 dir = getDirection (origen.me);
+
+				for (int i = 0; i < 10; i++) {
+					temporal.move (dir);
+					yield return new WaitForSeconds (0.1f);
+				}
+				unidades = me.getUnits ();
+				do {
+					numero_de_dados = temporal.getUnits ();
+
+					if (unidades == 0) {
+						me.conquer (origen.me.getOwner (), me.getUnits ());
+
+					} else {
+
+						if (unidades > 2)
+							unidades = 2;
+
+						numero_de_dados += unidades;
+
+						DiceSwipeControl.set_num_dices (numero_de_dados);
+						GameObject diceControl = GameObject.Find ("SwipeController");
+						diceControl.GetComponent<DiceSwipeControl> ().manualStart ();
+
+						//Recovering dices result
+						yield return new WaitForSeconds (10.0f);
+						bool stop = true;	
+
+						List<int> resultados = DiceSwipeControl.results;
+						if (resultados == null || resultados.Count != numero_de_dados) {
+
+							while (stop) {
+
+								yield return new WaitForSeconds (0.2f);
+								resultados = DiceSwipeControl.results;
+								if (resultados != null && resultados.Count == numero_de_dados) {
+									stop = false;
+									foreach (int resu in resultados) {
+										Debug.Log ("resu " + resu);			
+									}
+								}
+							}
+							print ("resultado if tile " + resultados.Count);
+						} else {
+							print ("resultado tile " + resultados.Count + " " + numero_de_dados);
+
+							int breakpoint = resultados.Count / 2;
+							int[] attacker = new int[breakpoint];
+							int[] defender = new int[breakpoint];
+							bool def = true;
+
+							for (int i = 0; i < breakpoint; i++) {
+
+								for (int j = breakpoint; j < breakpoint * 2; j++) {
+									if (resultados [i] >= resultados [j]) {
+										def = false;
+										break;
+									}
+								}
+								if (def == false) {
+									me.kill_unit ();
+									unidades--;
+								} else {
+									temporal.kill_unit ();
+								}
+
+								def = true;
+							}
+						}
+
+					}
+				} while(temporal.getUnits () != 0 && me.getUnits () != 0);
+
+				if (temporal.getUnits () == 0) {
+					Debug.Log ("Pierdes");
+				} else {
+					Debug.Log ("Ganas");
+					me.conquer (origen.me.getOwner (), temporal.getUnits ());
+				}
+				me.reset_hold ();
+				temporal.deinstantiate ();
+
+				paintUnits ();
+				Tile.reset_origen ();
+				Debug.Log ("Deseleccionada la casilla");
+			} else if (((string)main_behavior.jugadores [main_behavior.index_player]).Contains (me.getOwner ()) != true && Tile.origen == null) {
+				/*Do nothing, it's an error*/
+				Debug.Log ("No se pueden seleccionar casillas rivales!");
 			}
-			me.reset_hold ();
-			temporal.deinstantiate ();
-
-			paintUnits ();
-			Tile.reset_origen ();
-			Debug.Log ("Deseleccionada la casilla");
-		} else if (((string)main_behavior.jugadores [main_behavior.index_player]).Contains (me.getOwner ()) != true && Tile.origen == null) {
-			/*Do nothing, it's an error*/
-			Debug.Log ("No se pueden seleccionar casillas rivales!");
+			Tile.performing = false;
+		} else {
+			Debug.Log ("Performing action");
 		}
 		yield return 0;
 		//StartOptions.partida.FinPartida ();
