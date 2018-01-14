9b06c12af9a025ae042f9c4c44f183a3e4ca3a6e
AZayat <anazayatmata@gmail.com>
2018-01-14 20:14:03 +0100

    Script que mueve la camara a derecha e izquierda

---
+++ Assets/Game Jam Template/Scripts/ZoomCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomCamera : MonoBehaviour {
+
+	public float speed = 50f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos = transform.position;
+
+		if (Input.GetAxis("Mouse ScrollWheel") > 0)
+		{
+			pos.z += speed * Time.deltaTime;
+			/*
+			if (GetComponent<Camera> ().fieldOfView > 1)
+			{
+				GetComponent<Camera> ().fieldOfView--;
+			}
+			*/
+		}
+		if (Input.GetAxis("Mouse ScrollWheel") < 0)
+		{
+			pos.z -= speed * Time.deltaTime;
+			/*
+			if (GetComponent<Camera> ().fieldOfView < 100)
+			{
+				GetComponent<Camera> ().fieldOfView++;
+			}
+			*/
+		}
+		transform.position = pos;
+	}
+}
